e65363de8a9aef8fe8512b1391e9003b1d751c7c
chigirh <abch9431@gmail.com>
2024-06-24 22:34:49 +0900

    commit

--- src/components/MarkDownEditerPlain.tsx
+++ src/components/MarkDownEditerPlain.tsx
@@ -7,9 +7,11 @@ import { styled } from "@mui/material/styles";
 
 import Stack from "@mui/material/Stack";
 
+import PreviewIcon from '@mui/icons-material/Preview';
+import VerticalSplitSharpIcon from '@mui/icons-material/VerticalSplitSharp';
+import Divider from '@mui/material/Divider';
 import TextField from "@mui/material/TextField";
 import MarkDownViewer from "./MarkDownViewer";
-import SearchIcon from "@mui/icons-material/Search";
 
 import "easymde/dist/easymde.min.css";
 
@@ -28,17 +30,52 @@ const MarkDownEditorPlain: React.FC<ChapterCreateFormProp> = ({ props }) => {
 
   const [raw, setRaw] = useState(props.markdown);
   const [isView, setIsView] = useState(false);
+  const [isSplit, setIsSplit] = useState(false);
 
   return (
     <>
       <Container>
-        <Stack spacing={2}>
-          <Stack direction="row" spacing={2}>
-            <IconButton onClick={() => setIsView(!isView)}>
-              <SearchIcon></SearchIcon>
+        <Stack spacing={1} divider={<Divider flexItem />}>
+          <Stack direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
+            <IconButton onClick={() => {
+              if (isSplit) {
+                return
+              }
+              setIsView(!isView)
+            }}>
+              <PreviewIcon color={isView ? "action" : "disabled"} fontSize="small"></PreviewIcon>
+            </IconButton>
+            <IconButton onClick={() => {
+              setIsView(false)
+              setIsSplit(!isSplit)
+            }}>
+              <VerticalSplitSharpIcon color={isSplit ? "action" : "disabled"} fontSize="small"></VerticalSplitSharpIcon>
             </IconButton>
           </Stack>
-          {isView ? (
+          {isSplit ? (
+            <>
+              <Stack direction="row" spacing={0.2} divider={<Divider orientation="vertical" color="black" flexItem />}>
+                <Content>
+                  <TextField
+                    id="filled-multiline-static"
+                    multiline
+                    rows={10}
+                    defaultValue={raw}
+                    fullWidth
+                    onChange={(e) => onChange(e.target.value)}
+                    sx={{
+                      'fieldset': {
+                        border: 'none'
+                      }
+                    }}
+                  />
+                </Content>
+                <Content>
+                  <MarkDownViewer props={{ raw: raw }} />
+                </Content>
+              </Stack>
+            </>
+          ) : isView ? (
             <>
               <MarkDownViewer props={{ raw: raw }} />
             </>
@@ -50,8 +87,15 @@ const MarkDownEditorPlain: React.FC<ChapterCreateFormProp> = ({ props }) => {
               defaultValue={raw}
               fullWidth
               onChange={(e) => onChange(e.target.value)}
+              sx={{
+                'fieldset': {
+                  border: 'none'
+                }
+              }}
             />
-          )}
+          )
+          }
+
         </Stack>
       </Container>
     </>
@@ -62,7 +106,16 @@ const Container = styled("div")({
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  border: "solid 1px black",
+  border: "solid 1.5px grey",
+  padding: "10px",
+  borderRadius: "5px"
+});
+
+const Content = styled("div")({
+  width: "50%",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
   padding: "10px",
 });
 
